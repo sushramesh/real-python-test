6ba1827b6ec49be8e25d78623fc59d9c8e7ee2a2
Sushanth Ramesh <sushanth@live.in>
2015-10-12 14:06:34 +1100

    Fixed some grammar

--- poetry.py
+++ poetry.py
@@ -36,23 +36,36 @@ def make_poem():
 	adverb_3 = get_3_random(adverbs)
 
 	if vowels.find(adj_3[0][0]) != -1:
-		print "An {adjective1} {noun1}".format(adjective1=adj_3[0], noun1 = noun_3[0])
+		print "An {adjective1} {noun1}\n".format(adjective1=adj_3[0], noun1 = noun_3[0])
 	else:
-		print "A {adjective1} {noun1}".format(adjective1=adj_3[0], noun1 = noun_3[0])
+		print "A {adjective1} {noun1}\n".format(adjective1=adj_3[0], noun1 = noun_3[0])
 
-	if vowels.find(adj_3[0][0]) != -1:
-		print "An {adjective1} {noun1} {verb1} {preposition1} the {adjective2} {noun2}"\
-		.format(adjective1=adj_3[0], noun1=noun_3[0], verb1=verb_3[0], preposition1=prepo_3[0], \
-		adjective2=adj_3[1], noun2=noun_3[1])
-	else:
-		print "A {adjective1} {noun1} {verb1} {preposition1} the {adjective2} {noun2}"\
-		.format(adjective1=adj_3[0], noun1=noun_3[0], verb1=verb_3[0], preposition1=prepo_3[0], \
-		adjective2=adj_3[1], noun2=noun_3[1])
+	for i in range(0,5):
 
-	print "{adverb1}, the {noun1} {verb2}".format(adverb1=adverb_3[0], noun1=noun_3[0], verb2=verb_3[1])
-	print "the {noun2} {verb3} {preposition2} a {adjective3} {noun3}".format(noun2=noun_3[1], verb3=verb_3[2],\
-	preposition2=prepo_3[1], adjective3=adj_3[2], noun3=noun_3[2])
-	print "\n"
+		if vowels.find(adj_3[0][0]) != -1:
+			print "An {adjective1} {noun1} {verb1} {preposition1} the {adjective2} {noun2}"\
+			.format(adjective1=adj_3[0], noun1=noun_3[0], verb1=verb_3[0], preposition1=prepo_3[0],
+			adjective2=adj_3[1], noun2=noun_3[1])
+		else:
+			print "A {adjective1} {noun1} {verb1} {preposition1} the {adjective2} {noun2}"\
+			.format(adjective1=adj_3[0], noun1=noun_3[0], verb1=verb_3[0], preposition1=prepo_3[0],
+			adjective2=adj_3[1], noun2=noun_3[1])
 
-for i in range(0,4):
-	make_poem()
+		print "{adverb1}, the {noun1} {verb2}".format(adverb1=adverb_3[0], noun1=noun_3[0], verb2=verb_3[1])
+
+		if vowels.find(adj_3[2][0]) != -1:
+			print "the {noun2} {verb3} {preposition2} an {adjective3} {noun3}"\
+			.format(noun2=noun_3[1], verb3=verb_3[2],preposition2=prepo_3[1], adjective3=adj_3[2], noun3=noun_3[2])
+		else:
+			print "the {noun2} {verb3} {preposition2} an {adjective3} {noun3}"\
+			.format(noun2=noun_3[1], verb3=verb_3[2],preposition2=prepo_3[1], adjective3=adj_3[2], noun3=noun_3[2])
+
+		print "\n"
+
+		noun_3 = get_3_random(nouns)
+		verb_3 = get_3_random(verbs)
+		adj_3 = get_3_random(adjectives)
+		prepo_3 = get_3_random(prepositions)
+		adverb_3 = get_3_random(adverbs)
+
+make_poem()
